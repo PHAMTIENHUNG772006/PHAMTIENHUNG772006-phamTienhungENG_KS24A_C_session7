1dc7515177406f43a3fe42934f07f7c0e0522596
PHAMTIENHUNG772006 <hungpham772006@gmail.com>
2024-11-28 11:12:09 +0700

    Add files via upload

---
+++ sesion7_bt8.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+	int row, col;
+	printf("hay nhap so hang cua ma tran : ");
+	scanf("%d", &row);
+	printf("hay nhap so cot cua ma tran : ");
+	scanf("%d", &col);
+	if( row < 1 || col < 1){
+		printf("moi ban nhap lai du lieu ");
+		return 1;
+		
+	}
+	int maTran[row][col];
+	
+	for ( int i = 0 ; i < row ; i++){
+		for ( int j = 0 ; j < col ; j++){
+			printf(" Nhap phan tu thu %d cua hang %d: ",j+1, i+1);
+			scanf("%d", &maTran[i][j]);
+    } 
+    printf("\n");
+        }
+        printf("cac phan tu trong mang la : \n");
+        for ( int i = 0 ; i < row ; i++){
+		    for ( int j = 0 ; j < col ; j++){
+			printf(" \t%d ",maTran[i][j]);
+	}
+	printf("\n");
+	
+ }
+	return 0;
+}
